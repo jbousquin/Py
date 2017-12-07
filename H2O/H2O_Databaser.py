46313c0f7c8f9e973cefd0c546d54766e22d0ed5
Justin Bousquin <bousquin.justin@epa.gov>
2017-12-07 09:49:01 -0600

    initial writing

--- H2O/H2O_Databaser.py
+++ H2O/H2O_Databaser.py
@@ -6,16 +6,46 @@ bousquin.justin@epa.gov
 """
 
 import os
+import json
 import utils
 
 def soils():
-
+    #get soils
+    
 def landuse():
-
+    #export raster image
 def NHD():
-
-def roads():
-
+    #adapt what was used for RBI
+def roads(FIPS, directory):
+    # Download
+    filename = "tl_2013_{}_roads.zip".format(FIPS)
+    request = "ftp://ftp2.census.gov/geo/tiger/TIGER2013/ROADS/"
+    utils.HTTPS_download(request, directory, filename)
+    # Make sure download is valid
+    utils.Check_archive(directory, filename)
+    # Unzip
+    z = directory + os.sep + filename
+    utils.WinZip_unzip(z)
 
 def main():
-    
+    soils()    
+
+#try:
+#    main()
+#except:
+
+# Polygon from user
+poly = r"C:\ArcGIS\Local_GIS\Export_Output.shp"
+outDIR = os.path.dirname(poly)
+
+# Project in WebMercator (3857) if not projected
+
+
+county, FIPS = polyFIPS(poly)
+
+roads(FIPS, outDIR)
+
+
+
+# Get county for extent
+
