22a41a17f76c39a2c463fbb1106256393b23dc06
Justin Bousquin <bousquin.justin@epa.gov>
2017-12-13 10:10:40 -0600

    coded roads(), soils()

--- H2O/H2O_Databaser.py
+++ H2O/H2O_Databaser.py
@@ -9,26 +9,85 @@ import os
 import json
 import utils
 
-def soils():
-    #get soils
+def soils(FIPS, outDIR):
+    #current methods defines FIPS using current census, alternatively
+    #the polygon geometry can be used to define the SSA based on overlap.
+    #If re-writing this way, BE AWARE of Spatial Reference
+    #"soil data develppment tools" uses AOI to get Areasymbols
     
-def landuse():
-    #export raster image
-def NHD():
-    #adapt what was used for RBI
-def roads(FIPS, directory):
-    # Download
-    filename = "tl_2013_{}_roads.zip".format(FIPS)
-    request = "ftp://ftp2.census.gov/geo/tiger/TIGER2013/ROADS/"
-    utils.HTTPS_download(request, directory, filename)
-    # Make sure download is valid
-    utils.Check_archive(directory, filename)
-    # Unzip
-    z = directory + os.sep + filename
-    utils.WinZip_unzip(z)
+    # Build WSS query/download
+    # Define AOI by state/county
+    for FIP in FIPS:
+        state = utils.getStateName(FIP)
+        county = utils.splitFIPS(FIP)[1]
+        county_SSA = "{}{}".format(state, county)
+        
+        # SSA can be used to define area of interest on WSS
+        #http://websoilsurvey.nrcs.usda.gov/app/WebSoilSurvey.aspx?aoissa=NC175
+        map_aoi = "aoissa={}".format(SSA)
 
-def main():
-    soils()    
+        # Get list of SSA for FIP
+        SSA_list = utils.getCounty_surveys(FIP)
+        for SSA in SSA_list:
+            # Get latest saverest
+            date_year_mo_da = utils.getSurvey_date(SSA)            
+    
+            # Download SSURGO by SSA
+            #https://websoilsurvey.sc.egov.usda.gov/DSD/Download/Cache/SSA/wss_SSA_FL113_[2017-10-06].zip
+            WSS_url = "https://websoilsurvey.sc.egov.usda.gov/DSD/Download/Cache/SSA/"
+            filename = "wss_SSA_{}_[{}].zip".format(SSA, date_year_mo_da)
+            # Get zip
+            utils.HTTPS_download(WSS_url, directory, filename)
+            # Check zip
+            if utils.Check_archive(directory, filename):
+                z = directory + os.sep + filename
+                utils.WinZip_unzip(z)
+
+            # Instead get geometries and create polygons?
+
+
+##    #define AOI by envelope
+##    """This method is tricky because the geometry must be converted to their SR
+##    """
+##    aoi = "aoicoords=(({}))".format(utils.getEnvelope(poly))
+##
+##    #use mapserver
+##    """The method doesn't return geometries?
+##    """
+##    # Query mukey for polygons intersecting poly envelope
+##    geo = utils.getEnvelope(poly)
+##    geoType = "esriGeometryEnvelope"
+##    inSR = utils.getSR(poly)
+##    fields = ["Geometry", "mukey"]
+##    query = utils.geoQuery(geo, geoType, inSR, fields)
+##    
+##    #https://server.arcgisonline.com/arcgis/rest/services/Specialty/Soil_Survey_Map/MapServer/0
+##    catalog = "server.arcgisonline.com"
+##    service = "Specialty/Soil_Survey_Map"
+##    layer = "0"
+##    response = utils.MapServerRequest(catalog, service, layer, query)
+        
+def landuse():
+    #import default nlcd raster image
+    #https://datagateway.nrcs.usda.gov/GDGOrder.aspx - download by state?
+def NHD():
+    #adapt RBI Tool
+def roads(FIPS, directory):
+    for FIP in FIPS:
+        # Download
+        filename = "tl_2013_{}_roads.zip".format(FIP)
+        request = "ftp://ftp2.census.gov/geo/tiger/TIGER2013/ROADS/"
+        utils.HTTPS_download(request, directory, filename)
+        # Make sure download is valid
+        utils.Check_archive(directory, filename)
+        # Unzip
+        z = directory + os.sep + filename
+        utils.WinZip_unzip(z)
+
+def main(poly, outDIR):
+    county, FIPS = utils.polyFIPS(poly)
+    soils(FIPS, outDIR)
+    roads(FIPS, outDIR)
 
 #try:
 #    main()
@@ -36,16 +95,6 @@ def main():
 
 # Polygon from user
 poly = r"C:\ArcGIS\Local_GIS\Export_Output.shp"
-outDIR = os.path.dirname(poly)
-
+outDIR = r"C:\ArcGIS\Local_GIS\H2O\soil\auto_test"
 # Project in WebMercator (3857) if not projected
-
-
-county, FIPS = polyFIPS(poly)
-
-roads(FIPS, outDIR)
-
-
-
-# Get county for extent
-
+main(poly, outDIR)
